cad792e6c43f8a1fb1c2caa92b791439945f1e52
彭建波 <pengjianbosoft@gmail.com>
2016-01-05 21:39:50 +0800

    修复glide setTag问题

--- app/src/main/java/cn/finalteam/galleryfinal/sample/loader/GlideImageLoader.java
+++ app/src/main/java/cn/finalteam/galleryfinal/sample/loader/GlideImageLoader.java
@@ -18,11 +18,13 @@ package cn.finalteam.galleryfinal.sample.loader;
 
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
-
+import cn.finalteam.galleryfinal.sample.R;
+import cn.finalteam.galleryfinal.widget.GFImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-
-import cn.finalteam.galleryfinal.widget.GFImageView;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.Request;
+import com.bumptech.glide.request.target.ImageViewTarget;
 
 /**
  * Desction:
@@ -32,7 +34,7 @@ import cn.finalteam.galleryfinal.widget.GFImageView;
 public class GlideImageLoader implements cn.finalteam.galleryfinal.ImageLoader {
 
     @Override
-    public void displayImage(Activity activity, String path, GFImageView imageView, Drawable defaultDrawable, int width, int height) {
+    public void displayImage(Activity activity, String path, final GFImageView imageView, Drawable defaultDrawable, int width, int height) {
         Glide.with(activity)
                 .load("file://" + path)
                 .placeholder(defaultDrawable)
@@ -41,7 +43,22 @@ public class GlideImageLoader implements cn.finalteam.galleryfinal.ImageLoader {
                 .diskCacheStrategy(DiskCacheStrategy.NONE) //不缓存到SD卡
                 .skipMemoryCache(true)
                 //.centerCrop()
-                .into(imageView);
+                .into(new ImageViewTarget<GlideDrawable>(imageView) {
+                    @Override
+                    protected void setResource(GlideDrawable resource) {
+                        imageView.setImageDrawable(resource);
+                    }
+
+                    @Override
+                    public void setRequest(Request request) {
+                        imageView.setTag(R.id.adapter_item_tag_key,request);
+                    }
+
+                    @Override
+                    public Request getRequest() {
+                        return (Request) imageView.getTag(R.id.adapter_item_tag_key);
+                    }
+                });
     }
 
     @Override
